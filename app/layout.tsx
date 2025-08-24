22e48211ac573b72eb54cf7e2a25be3dfcfc3d0b
vinitshirbhate <vinit.shirbhate23@vit.edu>
2025-08-24 15:51:38 +0530

    clerk added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
+import { type Metadata } from 'next'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import './globals.css'
+
 import type React from "react"
-import type { Metadata } from "next"
 import { DM_Sans } from "next/font/google"
 import "./globals.css"
 
@@ -15,7 +25,7 @@ export const metadata: Metadata = {
   description:
     "Comprehensive geographical crime heat map dashboard for India with advanced filtering and socio-economic insights",
   generator: "v0.app",
-}
+} 
 
 export default function RootLayout({
   children,
@@ -23,8 +33,25 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${dmSans.variable} antialiased`}>
-      <body className="font-sans">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${dmSans.variable} ${dmSans.variable} antialiased`}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
